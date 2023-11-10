dd93a0270b9df18bc7167f95c45e97f5307c8d7f
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 05:00:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", 6769849216:AAGPVUHLAzt7p9pFldV03v2YYGjyE0sEZHQ) # Dapatkan di @botfather
+TOKEN = os.environ.get("TOKEN", "6769849216:AAGPVUHLAzt7p9pFldV03v2YYGjyE0sEZHQ") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "5821871362") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "-1001836288852") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "-1001844291346") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi

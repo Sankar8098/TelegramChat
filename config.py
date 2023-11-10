a147a25efd00b95e3a399dd8714d74ca7746e151
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 04:32:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa 
 GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "")
+
+
+
+hh

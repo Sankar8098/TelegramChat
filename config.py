9d42f4ac29466a26b137d93d8ff9115241170a73
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 04:54:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
-DB_URI = os.environ.get("DATABASE_URL", "")
-
-
-
-hh
+TOKEN = os.environ.get("TOKEN", 6769849216:AAGPVUHLAzt7p9pFldV03v2YYGjyE0sEZHQ) # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "5821871362") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "-1001836288852") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "-1001844291346") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://autofilter:autofilter@autofilter.tponhhw.mongodb.net/?retryWrites=true&w=majority"
+")

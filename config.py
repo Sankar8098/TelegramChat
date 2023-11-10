f1dddcff15397d376c2331bfaab74634364feee3
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 05:07:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 TOKEN = os.environ.get("TOKEN", "6769849216:AAGPVUHLAzt7p9pFldV03v2YYGjyE0sEZHQ") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "5821871362") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "-1001836288852") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "-1001844291346") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "SK_HD_MOVIE") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://autofilter:autofilter@autofilter.tponhhw.mongodb.net/?retryWrites=true&w=majority")

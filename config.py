bc293f95840ece1a6c6758b510aaa114d69e63a6
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-11-11 04:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ TOKEN = os.environ.get("TOKEN", 6769849216:AAGPVUHLAzt7p9pFldV03v2YYGjyE0sEZHQ) 
 OWNER = os.environ.get("OWNER", "5821871362") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "-1001836288852") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "-1001844291346") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://autofilter:autofilter@autofilter.tponhhw.mongodb.net/?retryWrites=true&w=majority"
-")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://autofilter:autofilter@autofilter.tponhhw.mongodb.net/?retryWrites=true&w=majority")
